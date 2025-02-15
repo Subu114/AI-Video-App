57ec0171b209f7ad7b97d1df726cf5a22a5cf678
Subu114 <21co63@aitdgoa.edu.in>
2025-02-15 17:35:19 +0530

    Added option to search by query

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, RefreshControl } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useEffect } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+
+import SearchInput from '../../components/SearchInput'
+import ExmptyState from '../../components/ExmptyState'
+import VideoCard from '../../components/VideoCard'
+import useAppwrite from '../../lib/useAppwrite'
+import {searchPosts } from '../../lib/appwrite'
 
 const Search = () => {
+  const { query } = useLocalSearchParams()
+  const { data: posts, loading, refetch } = useAppwrite(() => searchPosts(query))
+
+  useEffect(() => {
+    refetch()
+  }, [query])
+
+    console.log(posts)
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Result
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">
+              {query}
+            </Text>
+
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query} />
+            </View>
+
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <ExmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search..."
+          />
+
+        )}
+
+        refreshControl={<RefreshControl />}
+      />
+    </SafeAreaView>
   )
 }
 
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,30 +1,42 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { icons } from '../constants'
+import { router, usePathname } from 'expo-router'
 
-const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
-    const [showPassword, setShowPassword] = useState(false)
-    const [isFocused, setIsFocused] = useState(false)
+const SearchInput = ({ initialQuery }) => {
+    const pathName = usePathname()
+    const [query, setQuery] = useState(initialQuery || "")
+    const onSearch = () => {
+        if (!query)
+            return Alert.alert("Missing query! Please input something to search...")
+        if (pathName.startsWith('/search'))
+            router.setParams({ query })
+        else
+            router.push(`/search/${query}`)
+    }
+
     return (
         <View
-            className={`w-full h-16 px-4 bg-black-100 border-2 rounded-2xl ${isFocused ? 'border-secondary-200' : 'border-black-200'} items-center flex-row space-x-4`}
+            className={`w-full h-16 px-4 bg-black-100 border-2 rounded-2xl border-black-200 items-center flex-row space-x-4`}
         >
             <TextInput
-                placeholder="Seach for a video topic" 
-                value={value}
-                onChangeText={handleChangeText}
+                placeholder="Seach for a video topic"
+                value={query}
                 className="text-base mt-0.5 text-white flex-1 font-pregular"
-                placeholderTextColor="#7B7B8B"
-
+                placeholderTextColor="#CDCDE0"
+                onChangeText={(e) => setQuery(e)}
+                onSubmitEditing={onSearch}
             />
-            <TouchableOpacity onPress={() => {}}>
+            <TouchableOpacity
+                onPress={onSearch}
+            >
                 <Image
                     source={icons.search}
                     className="w-5 h-5"
                     resizeMode='contain'
                 />
             </TouchableOpacity>
-        </View>
+        </View >
     )
 }
 
