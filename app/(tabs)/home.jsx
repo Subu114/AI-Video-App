ff138aa1fed293b04805f871cfb3d279fa1da380
Subu114 <21co63@aitdgoa.edu.in>
2025-02-23 11:22:14 +0530

    fetvhing username from globalcontext

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,8 +8,11 @@ import ExmptyState from '../../components/ExmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
+  const { setIsLoggedIn, user, setUser } = useGlobalContext()
+
   const { data : posts, loading, refetch } = useAppwrite(getAllPosts)
   const { data : latestPost } = useAppwrite(getLatestPosts)
   const [refreshing, setRefreshing] = useState(false)
@@ -33,10 +36,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome to the Home Page
+                  Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Subudd
+                  {user?.username}
                 </Text>
 
               </View>
